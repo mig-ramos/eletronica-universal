59c86fe41ae27c9ecdd97722d63180446a92b5f7
mig-ramos <mig.ramos10@gmail.com>
2019-12-01 22:50:56 -0300

    Alteracao

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -29,7 +29,7 @@ instance Yesod App where
     isAuthorized HomeR _ = return Authorized
     isAuthorized EntrarR _ = return Authorized
     isAuthorized UsuarioR _ = return Authorized 
-    -- isAuthorized (StaticR _) _ = return Authorized
+    isAuthorized (StaticR _) _ = return Authorized
     isAuthorized AdminR _ = isAdmin
     isAuthorized _ _ = isUsuario
 
